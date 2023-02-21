9edb53235b764921fc282b9b7a7ac085c29cf65a
zhangyimeng220424 <125980124+zhangyimeng220424@users.noreply.github.com>
2023-02-21 20:50:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0sRRM2vSiTsiukvMoLS3AXq5T_fLXmL-LeuHU928K-o',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oPU585lFNRd6WJelFej8JxqP8Qmg',
+      id: '',
     }
   ],
 
