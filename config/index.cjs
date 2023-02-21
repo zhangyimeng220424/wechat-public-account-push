5d767ceea856e5d7ad4535d0972aa9992c3f9d7f
zhangyimeng220424 <125980124+zhangyimeng220424@users.noreply.github.com>
2023-02-21 22:48:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oPU585lFNRd6WJelFej8JxqP8Qmg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0sRRM2vSiTsiukvMoLS3AXq5T_fLXmL-LeuHU928K-o',
+      useTemplateId: 'gPGLHYeWjf1FAYa-nrq6ZgYr0IV3tmFZFE-IPUUmzag',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-13',
       festivals: [
